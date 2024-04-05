64e903020f9586d91f5e8ff586b9508e3aef35cb
jonpe <jonpettrich@gmail.com>
2024-04-05 23:06:52 +0200

    Improved UI and prepared code for easy component switching

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   let [activeplayerid, setActivePlayerID] = useState(0)
   var [players, setPlayers] = useState(['Player1', 'Player2', 'Player3']) //contains all playernames
   var [scores, setScores] = useState([startfrom, startfrom, startfrom])
+  let [showscorepad, setShowScorePad] = useState(true)
 
   let [shotcount, setShotCount] = useState(0) //represents first, second or third shot
   let [threedartscores, setThreeDartScores] = useState(["1st","2nd","3rd"]); //saves all three scores combined
@@ -33,6 +34,7 @@ function App() {
       {
         //document.getElementById('scorepad').style.display ='none';
         document.getElementById('continuegamebtn').style.display ='inline';
+        setShowScorePad(false)
       }
     }
     else { //if the input is 'back', go back to the last move
@@ -162,12 +164,14 @@ function App() {
     for(let i = 0; i<players.length; i++) { //resets all player profiles to standard color
       document.getElementById('player' + i).style.backgroundColor = '#35373c'
     }
-    document.getElementById('player0').style.backgroundColor = '#198754'
+    document.getElementById('player0').style.backgroundColor = '#198754' //sets first player to highlighted
   }
 
   function continueGame() { //shows the scorepad and hides the nextplayerbutton
     //document.getElementById('scorepad').style.display ='inline';
     document.getElementById('continuegamebtn').style.display ='none';
+    setShowScorePad(true)
+
     setShotCount(0)
     updateMultiplicator(1)
     setThreeDartScores(["1st","2nd","3rd"])
@@ -277,7 +281,7 @@ function PlayerProfile({id, name,score}) {
   );
 }
 
-function ScorePad({handleClick, updateMultiplicator, threedartscores}) {   
+function ScorePad({handleClick, updateMultiplicator, threedartscores}) { //holds three dart history and input pad
   return (
     <>
     <div id='scorepad'>
@@ -286,49 +290,64 @@ function ScorePad({handleClick, updateMultiplicator, threedartscores}) {
         <p className='dartscores' id='seconddart'>{threedartscores[1]}</p>
         <p className='dartscores' id='thirddart'>{threedartscores[2]}</p>
       </div>
-      <div className='scorepadrow'>
-        <ScorePadBtn value='1' classname='scorepadbtn standardbtn leftupperbtn' onScorePadBtnClick={()=> handleClick(1)}/>
-        <ScorePadBtn value='2' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(2)}/>
-        <ScorePadBtn value='3' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(3)}/>
-        <ScorePadBtn value='4' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(4)}/>
-        <ScorePadBtn value='back' classname='scorepadbtn standardbtn rightupperbtn backbtn' onScorePadBtnClick={()=> handleClick('back')}/>
-      </div>
-      <div className='scorepadrow'>
-        <ScorePadBtn value='5' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(5)}/>
-        <ScorePadBtn value='6' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(6)}/>
-        <ScorePadBtn value='7' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(7)}/>
-        <ScorePadBtn value='8' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(8)}/>
-        <ScorePadBtn value='back' classname='scorepadbtn standardbtn backbtn' onScorePadBtnClick={()=> handleClick('back')}/>
-      </div>
-      <div className='scorepadrow'>
-        <ScorePadBtn value='9' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(9)}/>
-        <ScorePadBtn value='10' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(10)}/>
-        <ScorePadBtn value='11' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(11)}/>
-        <ScorePadBtn value='12' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(12)}/>
-        <ScorePadBtn value='0' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(0)}/>
-      </div>
-      <div className='scorepadrow'>
-        <ScorePadBtn value='13' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(13)}/>
-        <ScorePadBtn value='14' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(14)}/>
-        <ScorePadBtn value='15' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(15)}/>
-        <ScorePadBtn value='16' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(16)}/>
-        <ScorePadBtn value='25' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(25)}/>
-      </div>
-      <div className='scorepadrow'>
-        <ScorePadBtn value='17' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(17)}/>
-        <ScorePadBtn value='18' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(18)}/>
-        <ScorePadBtn value='19' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(19)}/>
-        <ScorePadBtn value='20' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(20)}/>
-        <ScorePadBtn value='50' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(50)}/>
-      </div>  
-      <div className='scorepadrow'>
-        <ScorePadBtn value='DOUBLE' classname='scorepadbtn multibtn leftlowerbtn' onScorePadBtnClick={()=> updateMultiplicator(2)}/>
-        <ScorePadBtn value='TRIPLE' classname='scorepadbtn multibtn rightlowerbtn' onScorePadBtnClick={()=> updateMultiplicator(3)}/>
-      </div>
+      <ScoreValuePad handleClick={handleClick} updateMultiplicator={updateMultiplicator}/>
     </div>
     </>
   );
 }
+
+function ScoreValuePad({handleClick, updateMultiplicator}) {
+  return (
+    <div id='numrows'>
+        <div className='scorepadrow'>
+          <ScorePadBtn value='1' classname='scorepadbtn standardbtn leftupperbtn' onScorePadBtnClick={()=> handleClick(1)}/>
+          <ScorePadBtn value='2' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(2)}/>
+          <ScorePadBtn value='3' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(3)}/>
+          <ScorePadBtn value='4' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(4)}/>
+          <ScorePadBtn value='back' classname='scorepadbtn standardbtn rightupperbtn backbtn' onScorePadBtnClick={()=> handleClick('back')}/>
+        </div>
+        <div className='scorepadrow'>
+          <ScorePadBtn value='5' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(5)}/>
+          <ScorePadBtn value='6' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(6)}/>
+          <ScorePadBtn value='7' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(7)}/>
+          <ScorePadBtn value='8' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(8)}/>
+          <ScorePadBtn value='back' classname='scorepadbtn standardbtn backbtn' onScorePadBtnClick={()=> handleClick('back')}/>
+        </div>
+        <div className='scorepadrow'>
+          <ScorePadBtn value='9' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(9)}/>
+          <ScorePadBtn value='10' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(10)}/>
+          <ScorePadBtn value='11' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(11)}/>
+          <ScorePadBtn value='12' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(12)}/>
+          <ScorePadBtn value='0' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(0)}/>
+        </div>
+        <div className='scorepadrow'>
+          <ScorePadBtn value='13' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(13)}/>
+          <ScorePadBtn value='14' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(14)}/>
+          <ScorePadBtn value='15' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(15)}/>
+          <ScorePadBtn value='16' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(16)}/>
+          <ScorePadBtn value='25' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(25)}/>
+        </div>
+        <div className='scorepadrow'>
+          <ScorePadBtn value='17' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(17)}/>
+          <ScorePadBtn value='18' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(18)}/>
+          <ScorePadBtn value='19' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(19)}/>
+          <ScorePadBtn value='20' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(20)}/>
+          <ScorePadBtn value='50' classname='scorepadbtn standardbtn' onScorePadBtnClick={()=> handleClick(50)}/>
+        </div>  
+        <div className='scorepadrow'>
+          <ScorePadBtn value='DOUBLE' classname='scorepadbtn multibtn leftlowerbtn' onScorePadBtnClick={()=> updateMultiplicator(2)}/>
+          <ScorePadBtn value='TRIPLE' classname='scorepadbtn multibtn rightlowerbtn' onScorePadBtnClick={()=> updateMultiplicator(3)}/>
+        </div>
+      </div>
+  );
+}
+
+function NextplayerBtn(continueGame) {
+  return (
+    <button id='nextplayerbtn' onClick={continueGame}>Next player...</button>
+  );
+}
+
 
 function ScorePadBtn({value, classname, onScorePadBtnClick}) {
   return (
